94bfe535b3fbbc521ed51d277a47d577914d4c79
Calil Brandão <cbrandao2001@gmail.com>
2024-03-18 16:46:36 -0300

    a little change

--- app.java
+++ app.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class app {
     public static void main(String[] args) {
         Films f1 = new Films("The Dark Knight", "2012", "Christopher Nolan");
+        Films f2 = new Films("Clockwor kOrange", "1979", "Stanley Kubrick");
 
         f1.bioPrint();
+        f2.bioPrint();
     }       
 }
 
