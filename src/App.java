b88f2a4bcf05f8f1003609e0ae992237ed659696
Calil Brandão <cbrandao2001@gmail.com>
2024-03-18 16:47:52 -0300

    little changes

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         Films f1 = new Films("The Dark Knight", "2012", "Christopher Nolan");
-        Films f2 = new Films("Clockwor kOrange", "1979", "Stanley Kubrick");
+        Films f2 = new Films("Clockwork Orange", "1979", "Stanley Kubrick");
 
         f1.bioPrint();
         f2.bioPrint();
