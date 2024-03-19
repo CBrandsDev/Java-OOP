c3a93359070e86c995b57ba8603759619ac78ce9
Calil Brandão <cbrandao2001@gmail.com>
2024-03-19 15:57:01 -0300

    Testing a databank maybe

--- src/App.java
+++ src/App.java
@@ -1,13 +1,33 @@
 package src;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("DataBank of films by gender");
+
+        List<Films> films = new ArrayList();
+        boolean loop = true;
+
+        while (loop) {
+            String title = rTitle(scanner);
+        }
+
+        String titles[];
+        String years[];
+        String directors[];
+        
+
         Films f1 = new Films("The Dark Knight", "2012", "Christopher Nolan");
         Films f2 = new Films("Clockwork Orange", "1979", "Stanley Kubrick");
 
-        f1.bioPrint();
-        f2.bioPrint();
+        f1.rTitle();
+
+        // f1.bioPrint();
+        // f2.bioPrint();
     }       
 }
 
--- src/Films.java
+++ src/Films.java
@@ -2,39 +2,35 @@ package src;
 import java.util.Scanner;
 
 public class Films {
+
+    Scanner scanner = new Scanner(System.in);
     private String title;
     private String year;
     private String director;
     
-
+    
     public Films(String title, String year, String director) {
         this.title = title;
         this.year = year;
         this.director = director;
     }
-
+    
+    public static String rTitle() {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a epic film");
+        String title = scanner.nextLine();
+        System.out.println(title);
+        return title; 
+        
+    }
+    
     public void bioPrint() {
         System.out.println("Title: " + title);
         System.out.println("Year: " + year);
         System.out.println("Director: " + director);
     }
-
-    // actors = new ArrayList<>();
-    Scanner scanner = new Scanner(System.in);
-
     
-    // public void setActors() {
-    //     String loop;
-    //     do {
-    //         System.out.println("Set a actor to this film: ");
-    //         String actors = scanner.nextLine();
-    //         actors.add(actor);
-    //         System.out.println(("Do you want add more actors? (Y/N)"));
-    //         loop = scanner.nextLine();
-    //     } while (loop.equalsIgnoreCase("Y"));
-    // }
-    // public ArrayList<String> getActors() {
-    //     return actors;
-    // }
 }
+
 
